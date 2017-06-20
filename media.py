00d3efbeeb77cfc24e90a9fbea58b1081f340833
ldean1203 <ldean1203@gmail.com>
2017-06-20 20:38:21 +0800

    submit

---
+++ media.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+class Movie():
+    '''The Movie class create a instance of movie,need the title of movie,the storyline,the poster image url and trailer url'''
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
